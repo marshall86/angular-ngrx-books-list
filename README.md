dacea63657fb76fb0bddc0fa2f088561c482b253
marshall86 <antonio11061986@gmail.com>
2023-02-22 11:56:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ngrx-books-list
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-urbxtn)
---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { selectBookCollection, selectBooks } from './state/books.selectors';
+import { BooksActions, BooksApiActions } from './state/books.actions';
+import { GoogleBooksService } from './book-list/books.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  books$ = this.store.select(selectBooks);
+  bookCollection$ = this.store.select(selectBookCollection);
+
+  constructor(private booksService: GoogleBooksService, private store: Store) {}
+
+  onAdd(bookId: string) {
+    this.store.dispatch(BooksActions.addBook({ bookId }));
+  }
+
+  onRemove(bookId: string) {
+    this.store.dispatch(BooksActions.removeBook({ bookId }));
+  }
+
+  ngOnInit() {
+    this.booksService
+      .getBooks()
+      .subscribe((books) => {
+          console.log(books);
+          this.store.dispatch(BooksApiActions.retrievedBookList({ books }))
+        }
+      );
+  }
+}
